34722ad64fa532f01da1da1fc713d3b24b5e0a5b
Manca.19321 <Manca.19321@LABI1-6PC06>
2023-03-27 08:35:45 +0200

    task1

---
+++ MancD_InfoTest/MancD_Candidato.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MancD_InfoTest
+{
+    public class MancD_Candidato
+    {
+        private int _MancD_Matricola;
+        private string _MancD_Nome;
+
+        public int MancD_Matricola
+        {
+            get { return _MancD_Matricola; }
+            set { _MancD_Matricola = value; }
+        }
+        public string MancD_Nome
+        {
+            get { return _MancD_Nome; }
+            set { _MancD_Nome = value; }
+        }
+        public MancD_Candidato()
+        {
+            MancD_Matricola = 0;
+            MancD_Nome = null;
+        }
+        public MancD_Candidato(int n)
+        {
+            MancD_Matricola = n;
+            MancD_Nome = null;
+        }
+        public MancD_Candidato(string s)
+        {
+            MancD_Matricola = 0;
+            MancD_Nome = s;
+        }
+        public MancD_Candidato(int n, string s)
+        {
+            MancD_Matricola = n;
+            MancD_Nome = s;
+        }
+        public abstract bool isIdoneo()
+        {
+            return true;
+        }
+        public abstract int punteggio()
+        {
+            return 0;
+        }
+    }
+}
