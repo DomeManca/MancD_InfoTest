e0d9940cf262d48ba4d1ef4b61166e93f8a0da51
Manca.19321 <Manca.19321@LABI1-6PC06>
2023-03-27 10:56:03 +0200

    task5

--- MancD_InfoTest/Form1.cs
+++ MancD_InfoTest/Form1.cs
@@ -15,6 +15,56 @@ namespace MancD_InfoTest
         public Form1()
         {
             InitializeComponent();
+            
+        }
+        List<MancD_Candidato> Candidati = new List<MancD_Candidato>();
+        public void Aggiunta(MancD_Candidato c)
+        {
+            Candidati.Add(c);
+        }
+        public string[] Visualizza()
+        {
+            string[] lista = new string[999];
+            int count = 0;
+            foreach (MancD_Candidato c in Candidati)
+            {
+                lista[count] = c.ToString();
+                count++;
+            }
+            return lista;
+        }
+        public void Modifica(string n, int m)
+        {
+            for (int i = 0; i < Candidati.Count; i++)
+            {
+                if (Candidati[i].MancD_Matricola == m)
+                {
+                    Candidati[i].MancD_Nome = n;
+                }
+            }
+        }
+        public void Elimina(int m)
+        {
+            for (int i = 0; i < Candidati.Count; i++)
+            {
+                if (Candidati[i].MancD_Matricola == m)
+                {
+                    Candidati[i] = null;
+                }
+            }
+        }
+        public string[] VisualizzaIdonei()
+        {
+            string[] lista = new string[999];
+            int count = 0;
+            foreach (MancD_Candidato c in Candidati)
+            {
+                if(c.isIdoneo())
+                    lista[count] = c.ToString();
+
+                count++;
+            }
+            return lista;
         }
     }
 }
